be3cab98088431db821db22b52bd724f0aa60c8b
Edwin Bekaert <48248357+EdwinBekaert@users.noreply.github.com>
2019-03-08 21:54:37 +0100

    when country code is NOT available call getEntByData()

--- src/main/kotlin/bl/venue/Srv.kt
+++ src/main/kotlin/bl/venue/Srv.kt
@@ -1,27 +1,31 @@
 package bl.venue
 
+import base.Args
 import base.Srv
-import base.whenRow
+import base.tryGetFirst
 import bl.Pkgs
 import bl.Req
 import data.Venues
 import org.jetbrains.exposed.sql.Query
 
-class Srv(private val mngr: Mngr) : Srv(mngr = mngr){
+class Srv(private val mngr: Mngr) : Srv(mngr = mngr) {
 
     override val primaryKey = Ent.primaryKey // gives base service access to id
 
     override fun whenEnt(objEnt: base.Ent, qryData: Query?, objReq: Req) {
-        with(objEnt as Ent){
-            whenDirty(objEnt::venueId.name){
+        with(objEnt as Ent) {
+            whenDirty(objEnt::venueId.name) {
                 println("id is dirty $venueId")
             }
-            // get country data which is NOT in qry, so fetch from countrySrv...
-            var countryCode: String = ""
-            qryData.whenRow { countryCode = it[Venues.country_code]!!}
-            val newArgs = args.plus(bl.country.Ent.primaryKey to countryCode)
-            country = mngr.getMngr(Pkgs.COUNTRY).getSrv().getEntByCode(objReq = objReq, args = newArgs) as bl.country.Ent
+            // get country data which is NOT in qryData, so fetch from countrySrv...
+            // when not null -> get country by id
+            // when no country code -> get country by data
+            country = qryData.tryGetFirst(Venues.country_code)?.let {
+                val newArgs: Args = args.plus(bl.country.Ent.primaryKey to it)
+                mngr.getMngr(Pkgs.COUNTRY).getSrv().getEntByCode(objReq = objReq, args = newArgs) as bl.country.Ent
+            } ?: mngr.getMngr(Pkgs.COUNTRY).getSrv().getEntByData(objReq = objReq, args = args) as bl.country.Ent
         }
     }
-
 }
+
+
