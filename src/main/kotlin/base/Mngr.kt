439e5dc4830efeef295b7b340683eb6b2f5c2d99
Edwin Bekaert <48248357+EdwinBekaert@users.noreply.github.com>
2019-03-10 22:02:59 +0100

    remove objName argument in getEnt()
    Instead of objName depend on type

--- src/main/kotlin/base/Mngr.kt
+++ src/main/kotlin/base/Mngr.kt
@@ -9,8 +9,8 @@ abstract class Mngr : Base(), MngrInterface {
 
     // cast in pkg-level mngr
     // pkg and name derived from typed
-    internal inline fun <reified E: Ent> getEntCasted(objReq: Req, args: Args, entIdField: String, qryData: Query? = null)
-            = objReq.getEnt(objName = E::class.java.canonicalName, args = args, entIdField = entIdField, qryData = qryData) as E
+    protected inline fun <reified E: Ent> getEntCasted(objReq: Req, args: Args, entIdField: String, qryData: Query? = null)
+            = objReq.getEnt<E>(args = args, entIdField = entIdField, qryData = qryData) as E
 
     // go get a manager from another package
     fun getMngr(pkgName: Pkgs) = App.getMngr(pkgName = pkgName)
--- src/main/kotlin/bl/Req.kt
+++ src/main/kotlin/bl/Req.kt
@@ -10,28 +10,26 @@ class Req {
     val entities = mutableMapOf<String, Ent>()
     val response by lazy { Response() }
 
-    fun getEnt(objName: String, args: Args, qryData: Query?, entIdField: String)
-            = entities.getOrPut(key = "$objName.${args.getOrDefault(entIdField, defaultValue = "init")}") {
-                createEnt(objName = objName, args = args, qryData = qryData)
-            }
+    inline fun <reified E : Ent> getEnt(args: Args, qryData: Query?, entIdField: String) = entities.getOrPut(key = "${E::class.java.canonicalName}.${args.getOrDefault(entIdField, defaultValue = "init")}") {
+        when (E::class.java.canonicalName) { // init with qryData if available else only with args
 
-    // TODO Streamline object factoring as in App
-    private fun createEnt(objName: String, args: Args, qryData: Query?) =
-        when (objName) {
-            // init with qryData if available
             bl.event.Ent::class.java.canonicalName
-                    -> qryData.whenNotNull { bl.event.Ent(qryData = it, args = args) }
+                -> qryData.whenNotNull { bl.event.Ent(qryData = it, args = args) }
                     ?: bl.event.Ent(args = args)
-            bl.event.AnotherEnt::class.java.canonicalName
-                    -> qryData.whenNotNull { bl.event.AnotherEnt(qryData = it, args = args) }
-                    ?: bl.event.AnotherEnt(args = args)
-            bl.venue.Ent::class.java.canonicalName
-                    -> qryData.whenNotNull { bl.venue.Ent(qryData = it, args = args) }
-                    ?: bl.venue.Ent(args = args)
-            bl.country.Ent::class.java.canonicalName
-                    -> qryData.whenNotNull { bl.country.Ent(qryData = it, args = args) }
-                    ?: bl.country.Ent(args = args)
-            else -> throw IllegalArgumentException("object $objName is not (yet) defined in Req.")
-        }
 
+            bl.event.AnotherEnt::class.java.canonicalName
+                -> qryData.whenNotNull { bl.event.AnotherEnt(qryData = it, args = args) }
+                    ?: bl.event.AnotherEnt(args = args)
+
+            bl.venue.Ent::class.java.canonicalName
+                -> qryData.whenNotNull { bl.venue.Ent(qryData = it, args = args) }
+                    ?: bl.venue.Ent(args = args)
+
+            bl.country.Ent::class.java.canonicalName
+                -> qryData.whenNotNull { bl.country.Ent(qryData = it, args = args) }
+                    ?: bl.country.Ent(args = args)
+
+            else -> throw IllegalArgumentException("object ${E::class.java.canonicalName} is not (yet) defined in Req.")
+        }
+    }
 }
