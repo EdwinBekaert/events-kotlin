0d48724590ad7e3cae151ad19e8719480428420b
Edwin Bekaert <48248357+EdwinBekaert@users.noreply.github.com>
2019-03-28 11:16:17 +0100

    tryGet is depreciated + add hasRow

--- src/main/kotlin/base/Extensions.kt
+++ src/main/kotlin/base/Extensions.kt
@@ -7,9 +7,10 @@ import org.jetbrains.exposed.sql.ResultRow
 // Execute a callback when variable is not null... avoid checking for nulls (.let trick)
 fun <T:Any, R> T?.whenNotNull(callback: (T)->R): R? = this?.let(callback)
 
-// runs callback for first row...
+// runs callback for first row
 fun Query?.whenRow(callback: (ResultRow)->Unit) = this?.firstOrNull()?.let(callback)
-
-// try get a column of first row or return null send TABLE.COLUM as expression
-fun <R> Query?.tryGetFirst(col: Expression<R>) = this?.firstOrNull()?.tryGet(col)
+// runs callback when we have at least one row
+fun <R> Query?.hasRow(callback: (Query) -> R): R? = this?.let{ it.takeIf { count() > 0}?.let(callback)}
+// try get a column of first row or return null send TABLE.COLUMN as expression
+fun <R> Query?.tryGetFirst(col: Expression<R>) = this?.firstOrNull()?.getOrNull(col)
 
